d85d4320426ecc2256f0c3ca0a866b344a227897
Artyom Korobkin <carlionminon@gmail.com>
2024-12-01 10:25:44 +0300

    UPD: Base

--- BD/BD/settings.py
+++ BD/BD/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7^bo%(x3ukxs2uqq=fqnk&l2vl!p4i$l3lo^brchr13)33_^kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['legacy-famcs.store']
+ALLOWED_HOSTS = ['legacy-famcs.store', '127.0.0.1']
 
 
 # Application definition
--- BD/services/tasks.py
+++ BD/services/tasks.py
@@ -4,3 +4,25 @@ from django.core.management import call_command
 @shared_task()
 def dbackup_task():
     call_command('dbackup')
+    
+import requests
+from celery import shared_task
+from django.db import connection
+
+@shared_task
+def update_users():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM users_user")
+        users = cursor.fetchall()
+
+    users_data = [
+        {desc[0]: user[i] for i, desc in enumerate(cursor.description)}
+        for user in users
+    ]
+
+    response = requests.post(
+        "https://legacy-famcs-stage.ru/api/users/update",
+        json={"users": users_data},
+    )
+
+    return response.status_code
